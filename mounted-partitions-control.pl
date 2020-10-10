832f320a5a04166a328d754a69d47924d7585d61
vuckale <56733276+vuckale@users.noreply.github.com>
2020-10-11 01:19:52 +0200

    if there is no mounted drive the script exits now

--- mounted-partitions-control.pl
+++ mounted-partitions-control.pl
@@ -6,6 +6,7 @@ open(DF, "df|");
 my @mount_points = <DF>;
 my @media_drives = ();
 my @media_drives_filesystem = ();
+
 for my $i (1 .. $#mount_points) {
     my @mounted_point_splitted = split ' ', $mount_points[$i];
     my $mounted_point_path = $mounted_point_splitted[5];
@@ -15,27 +16,28 @@ for my $i (1 .. $#mount_points) {
         push @media_drives, $parsed_media_drives[0];
     }
 }
-print "<ID>: <MOUNTED MEDIA>\n\n";
-for my $i (0 .. $#media_drives) {
-   my $shifted_index = $i + 1;
-   printf "%s: $media_drives[$i]\n", $i+1;
-   # print "$i: $media_drives_filesystem[$i]\n";
-}
-print "\nWhich drive do you want to unmount?
- Type <ID> and press ENTER key.\n";
-my $done = 0;
-while (!$done){
-    my $id = <STDIN>;
-    if ($id >= 1 && $id <= scalar(@media_drives)) {
-        print "$media_drives[$id-1] unmounted\n";
-        my $file_system = $media_drives_filesystem[$id-1];
-        my $command = "udisksctl unmount -b " . $file_system .
-         " && udisksctl power-off -b " . $file_system;
-        system $command;
-        $done = 1;
-    } else {
-        print "Entered <ID> does not belong to any
-         mounted drive. Try again.\n"
+if (scalar(@media_drives)){
+    print "<ID>: <MOUNTED MEDIA>\n\n";
+    for my $i (0 .. $#media_drives) {
+    my $shifted_index = $i + 1;
+    printf "%s: $media_drives[$i]\n", $i+1;
+    # print "$i: $media_drives_filesystem[$i]\n";
     }
+    print "\nWhich drive do you want to unmount? Type <ID> and press ENTER key.\n";
+    my $done = 0;
+    while (!$done){
+        my $id = <STDIN>;
+        if ($id >= 1 && $id <= scalar(@media_drives)) {
+            my $file_system = $media_drives_filesystem[$id-1];
+            my $command = "udisksctl unmount -b " . $file_system .
+            " && udisksctl power-off -b " . $file_system;
+            system $command;
+            $done = 1;
+        } else {
+            print "Entered <ID> does not belong to any mounted drive. Try again or press ^C to exit.\n"
+        }
+    }
+} else {
+    print "No mounted drive found. Exiting...\n"
 }
 close(DF);
