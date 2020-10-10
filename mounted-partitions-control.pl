400126e108280b9926079bb35316fb5156575e33
vuckale <56733276+vuckale@users.noreply.github.com>
2020-10-11 00:15:57 +0200

    print only mounted media drives

--- mounted-partitions-control.pl
+++ mounted-partitions-control.pl
@@ -3,9 +3,16 @@ use warnings;
 use diagnostics;
 open(DF, "df|");
 my @mount_points = <DF>;
+my @media_drives = ();
 for my $i (1 .. $#mount_points) {
     my @mounted_point_splitted = split ' ', $mount_points[$i];
     my $mounted_point_path = $mounted_point_splitted[5];
-    print "$i: $mounted_point_path\n"   
+    my @parsed_media_drives = grep(/media/, $mounted_point_path);
+    if (scalar(@parsed_media_drives)) {
+        push @media_drives, $parsed_media_drives[0];
+    }
+}
+for my $i (0 .. $#media_drives) {
+   print "$i: $media_drives[$i]\n"; 
 }
 close(DF);
