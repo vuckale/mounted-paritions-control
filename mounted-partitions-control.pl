0fa27ec9027b9309363d217a1d03e0f4351940e2
vuckale <56733276+vuckale@users.noreply.github.com>
2020-10-10 23:58:56 +0200

    renamed variables and blank README.md file

--- mounted-partitions-control.pl
+++ mounted-partitions-control.pl
@@ -4,8 +4,8 @@ use diagnostics;
 open(DF, "df|");
 my @mount_points = <DF>;
 for my $i (1 .. $#mount_points) {
-    my @directories = split ' ', $mount_points[$i];
-    my $parent_directory = $directories[5];
-    print "$i: $parent_directory\n"   
+    my @mounted_point_splitted = split ' ', $mount_points[$i];
+    my $mounted_point_path = $mounted_point_splitted[5];
+    print "$i: $mounted_point_path\n"   
 }
 close(DF);
